ecbd8886a0e6d75fb92a3bdde2453243c0461869
fuyunhuan <74292386+fuyunhuan@users.noreply.github.com>
2020-11-16 17:09:59 +0800

    完成登陆信息

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Login from '../components/login.vue'
+import Home from '../components/Home.vue'
 
 Vue.use(VueRouter)
 const routes = [
@@ -12,9 +13,20 @@ const routes = [
   {
     path: '/Login',
     component: Login
+  },
+  {
+    path: '/home',
+    component: Home
   }
 ]
 const router = new VueRouter({
   routes
+})
+// 挂载路由导航守卫
+router.beforeEach((to, from, next) => {
+ if (to.path === '/login') return next()
+ const tokenStr = window.sessionStorage.getItem('token')
+ if (!tokenStr) return next('/login')
+ next()
 })
 export default router
