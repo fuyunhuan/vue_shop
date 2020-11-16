2ea52fcf7ea99149a23a81a0ffac26ffa035f627
fuyunhuan <74292386+fuyunhuan@users.noreply.github.com>
2020-11-16 10:18:10 +0800

    1

--- src/components/login.vue
+++ src/components/login.vue
@@ -1,6 +1,10 @@
 <template>
     <div class="login_container" >
-      <div class="login_box" ></div>
+      <div class="login_box" >
+          <div class="avatar_box">
+            <img src="../assets/logo.png" alt="">
+          </div>
+      </div>
     </div>
 </template>
 
@@ -14,11 +18,15 @@
     background-color: #2b4b6b;
     height: 100%;
 }
-
 .login_box{
     width: 450px;
     height: 300px;
     background-color: #ffffff;
     border-radius: 3px;
+    position: absolute;
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%,-50%);
+    
 }
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,8 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Login from '../components/Login.vue'
+import Login from '../components/login.vue'
 
 Vue.use(VueRouter)
-
 const routes = [
   // 自动重定向
   {
@@ -15,9 +14,7 @@ const routes = [
     component: Login
   }
 ]
-
 const router = new VueRouter({
   routes
 })
-
 export default router
